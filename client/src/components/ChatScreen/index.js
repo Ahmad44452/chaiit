eb1dd8a29b21032985030e7add493bd79c776b1b
Ahmad44452 <ahmadghani44452@gmail.com>
2022-08-18 02:54:37 +0500

    css variables added

--- client/src/components/ChatScreen/index.js
+++ client/src/components/ChatScreen/index.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 import NavbarHoc from "../../hoc/NavbarHoc";
 
 const ChatScreen = () => {
+
+  const formik = useFormik();
+
   return (
     <NavbarHoc>
-      <h1>Chat Screen</h1>
+      <div className="chatScreen">
+        <div className="chatScreen__online">
+          <div className="chatScreen__online--user">Ahmad</div>
+          <div className="chatScreen__online--user">Nabeel</div>
+          <div className="chatScreen__online--user">Afzal</div>
+          <div className="chatScreen__online--user">Arslan</div>
+        </div>
+        <div className="chatScreen__chat">
+          <div className="chatScreen__chat--display">
+            <div className="chatScreen__chat--message">Hello</div>
+            <div className="chatScreen__chat--message">How are you?</div>
+          </div>
+          <form>
+            <input type="text" />
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
     </NavbarHoc>
   )
 }
