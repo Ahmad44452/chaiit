8e1f51c8276a4681066a3fe05267011d11e8ab1c
Ahmad44452 <ahmadghani44452@gmail.com>
2022-08-24 12:05:40 +0500

    minor changes

--- server/server.js
+++ server/server.js
@@ -41,10 +41,12 @@ app.use("/api/user", usersApi);
 
 app.use(express.static('client/build'));
 
-const path = require("path");
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
-})
+if (process.env.NODE_ENV === 'production') {
+  const path = require("path");
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+  })
+}
 
 
 socketIO.on('connection', (socket) => {
